53e24cab71dcd11f0306a36665aebecf6a70c778
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-06-01 22:46:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ class MarkdownTokenizer:
             self.tokens.append(NewLineToken())  # Add a NewLineToken for each new line
 
     def tokenize_line(self, line):
-        pos = 0
-
         # Header
         header_match = re.match(r'^(#{1,6})\s+(.*)', line)
         if header_match:
@@ -32,6 +30,14 @@ class MarkdownTokenizer:
             self.tokens.append(ListToken(value, ordered))
             self.tokenize_inline(value)  # Tokenize inline elements within the list item
             return
+        
+        block_quote_match = re.search(r'^> ?(.*)', line)
+        if block_quote_match:
+            value = block_quote_match.group(1)
+            self.tokens.append(BlockquoteToken(value))
+            self.tokenize_inline(value)
+            return
+
 
         # Process other inline elements
         self.tokenize_inline(line)
@@ -41,14 +47,17 @@ class MarkdownTokenizer:
         length = len(text)
         while pos < length:
             bold_match = re.search(r'\*\*(.*?)\*\*', text[pos:])
+            italic_match = re.search(r'\*(.*?)\*', text[pos:])
             link_match = re.search(r'\[(.*?)\]\((.*?)\)', text[pos:])
-            print(bold_match)
+
             if bold_match:
+                # handle text if theres something before bold match **hing****new** ll **www** kgj
                 if bold_match.start() > 0:
                     self.tokens.append(TextToken(text[pos:pos + bold_match.start()]))
                 self.tokens.append(BoldToken(bold_match.group(1)))
                 pos += bold_match.end()
             elif link_match:
+                # handle text if theres something before link match
                 if link_match.start() > 0:
                     self.tokens.append(TextToken(text[pos:pos + link_match.start()]))
                 self.tokens.append(LinkToken(link_match.group(1), link_match.group(2)))
@@ -65,11 +74,14 @@ markdown_text = """
 # Header 1
 ## Header 2
 Some **bold** text and a [link](http://example.com).
-    
+
+Directly 2 tokens **like** [this](http://example.com)    
 - Unordered list item
 1. Ordered list item
 2. Ordered **with** bold text! How **cool**!
+> Thats **cool** shit!
 """
+
 tokenizer = MarkdownTokenizer(markdown_text)
 tokens = tokenizer.get_tokens()
 for token in tokens:
